a4011653530c268f960925a965f2478b5e650d8f
Matthew Pallenik <mpallenik@gmail.com>
2023-12-07 11:44:34 -0500

    Filled out more of the curriculum database

--- java/database/curriculum.sql
+++ java/database/curriculum.sql
@@ -120,4 +120,124 @@ https://vuejs.org/guide/introduction.html |
 https://eslint.org/ |
 https://vitejs.dev/ |
 https://vuejs.org/guide/quick-start.html#creating-a-vue-application'
+),
+(
+'Variables and Data Types',
+
+'Typical programs like web applications need to store and manipulate data. Programmers try to write these applications with as little repetitive code as possible. For example, building a password reset flow for every single user is time consuming. So, to save time, programmers use variables to store the user''s email address. This allows them to write their code in a generic and reusable manner.',
+
+'variable | comment | line comment | data type | boolean | byte | short | int | long | float | double | char | string | declare | data-type identifier | camel-case | constant | expression | precedence | concatenate | type conversion | widen | narrow | literal',
+
+'https://lms.techelevator.com/cohorts/535/blocks/7/content_files/01_Variables_Data_Types/01-variables.md |
+https://docs.oracle.com/javase/tutorial/java/nutsandbolts/datatypes.html |
+https://docs.oracle.com/javase/tutorial/java/nutsandbolts/_keywords.html |
+https://docs.oracle.com/javase/specs/jls/se11/html/jls-15.html#jls-15.17.3'
+),
+(
+'Logical Branching',
+
+'As a programmer, you may come across a situation where your program must decide what to do next based on the data available to it. Some concepts that enable code to make decisions and determine what to do next include: boolean expressions, if/else statements, and methods.',
+
+'logical branching | boolean expressions | block | method | conditional code | scope | comparison | operator | if/else | XOR | ternary operator | nested block',
+
+'https://lms.techelevator.com/cohorts/535/blocks/7/content_files/02_Logical_Branching/01-introduction.md'
+),
+(
+'Loops and Arrays',
+
+'Arrays are a special data type found in almost all programming languages. They make it more convenient to work with values in aggregate rather than a series of individual variables. When you want to work with collections of values, you use arrays. Rather than creating multiple variables, you create a single variable name that can represent multiple values. You can create arrays to hold any data type. Loops provide a convenient way to write a small amount of code that can iterate through all of the values in an array one by one.',
+
+'array | collection | declare | initialize | length | element | index | notation | loop | for | while | do-while | initializer | condition | iterator | scope | break | continue | return type | name | parameter',
+
+'https://lms.techelevator.com/cohorts/535/blocks/7/content_files/03_Loops_and_Arrays/01-arrays.md'
+),
+(
+'Command-line Programs',
+
+'These days, when programs interact with a user, they usually do so through a graphical user interface (GUI) that provides visual controls like buttons and toggle switches. Not all programs have a GUI, though. Many system administration and developer tools interact only through text output to the screen and input from the keyboard. These are called command-line programs. At a high-level, a command-line program typically does three things: prompts the user to provide input, does something with that input, and outputs the result.',
+
+'command-line | program | user input | I/O | system.out.print | scanner | system.in | nextLine | parse',
+
+'https://lms.techelevator.com/cohorts/535/blocks/7/content_files/04_Command_Line_Programs/01-introduction.md'
+),
+(
+'Intro to Objects',
+
+'Object-Oriented Programming is about organizing your problem-solving into objects. An object is an in-memory data structure that combines state and behavior into a usable and useful abstraction. In other words, objects are a collection of variables and methods. Objects have many benefits: they''re modular, they hide information, they allow you to reuse code, and they''re pluggable and testable. To make objects, you must write classes. A class is to an object like a blueprint is to a house. A class defines the features of the objects created as instances of that class.',
+
+'abstraction | object | object-oriented programming | oop | modular | class | instantiation | constructor | stack | heap | memory address | string | length | substring | contains | index | toUpperCase | toLowerCase | BigDecimal | LocalDate',
+
+'https://lms.techelevator.com/cohorts/535/blocks/7/content_files/05_Intro_to_Objects_Strings/01-whats-an-object.md'
+),
+(
+'Collections (Part 1)',
+
+'The List<T> interface is the most common data structure used to handle collections in the Java language. Like an array, a List serves as a general-purpose, type-safe collection of values. The primary difference between the two is that Lists can grow and shrink in size, while arrays are always fixed in length. Another important distinction for Lists is they maintain the order in which items were added. The first item added is index 0, and the second is index 1. The Stack<T> class is similar to List in that it maintains the order in which items are added and removed, but it is far less commonly used than List. A Stack is a last-in, first-out (LIFO) data structure. Stacks allow you to "push" and "pop" items on and off the top of the collection. This is the one like a Pringles can. Like the Stack class, the Queue<T>interface is rarely used in general practice. A Queue is an "ordered" First-In First-Out (FIFO) data structure. New items are added at the end of the collection, and existing items are pulled from the beginning.',
+
+'collection | fixed | import | data structures | java.util.list | list | ArrayList | ordered | foreach | stack | queue | push | pop | LinkedList | offer | poll | add | remove | get',
+
+'https://lms.techelevator.com/cohorts/535/blocks/7/content_files/06_Collections_Part_1_Lists_Stacks_Queues/01-array-recap.md |
+https://docs.oracle.com/javase/tutorial/java/package/usepkgs.html |
+https://docs.oracle.com/javase/tutorial/collections/interfaces/index.html |
+https://docs.oracle.com/javase/tutorial/java/data/autoboxing.html |
+https://docs.oracle.com/en/java/javase/11/docs/api/java.base/java/util/List.html |
+https://docs.oracle.com/en/java/javase/11/docs/api/java.base/java/util/ArrayList.html |
+https://docs.oracle.com/javase/tutorial/essential/exceptions/ |
+https://docs.oracle.com/en/java/javase/11/docs/api/java.base/java/util/Stack.html |
+https://docs.oracle.com/en/java/javase/11/docs/api/java.base/java/util/Queue.html |
+https://docs.oracle.com/en/java/javase/11/docs/api/java.base/java/util/LinkedList.html'
+),
+(
+'Collections (Part 2)',
+
+'The Map class offers a distinct advantage over Lists. Maps consist of key-value pairs where each value is associated with a unique key, and the keys are arranged within the collection so that they can be instantly located and the values retrieved. There is no need to iterate through the collection looking for a particular value. A program should only loop through a Map when it needs to access every single value within. On the other hand, one disadvantage to Maps is they don''t maintain order in the sense of keeping track of the order in which items were added. Another unordered data structure is Set, a collection that holds unique values. It allows you to quickly determine if the value already exists in the set. Because Sets are unordered, they don''t maintain indexes. Items in a Set are identified by hashcodes. A hashcode is a specially computed value which uniquely identifies each item in the Set.',
+
+'binary search | map | key | value | put | get | containsKey | entry set | hashmap | set | hashcode | add | remove | hashset',
+
+'https://lms.techelevator.com/cohorts/535/blocks/7/content_files/07_Collections_Part_2_Maps/01-introduction.md |
+https://docs.oracle.com/en/java/javase/11/docs/api/java.base/java/util/Map.html |
+https://docs.oracle.com/en/java/javase/11/docs/api/java.base/java/util/HashMap.html |
+https://docs.oracle.com/en/java/javase/11/docs/api/java.base/java/util/Set.html |
+https://docs.oracle.com/en/java/javase/11/docs/api/java.base/java/util/HashSet.html'
+),
+(
+'Classes and Encapsulation',
+
+'Programmers often need to solve problems that the Java language doesn''t have an out-of-the-box solution for. To create these sorts of applications, programmers need to represent things like shopping carts and user profiles as custom data types. Using classes, they can define what characteristics and behaviors a data type has. Programmers can create as many instances of objects from the class blueprint as needed. The bundling of an object''s state and behaviors together is part of the definition of encapsulation, the first pillar of object-oriented programming.',
+
+'class | state | instance | blueprint | behavior | encapsulation | object-oriented-programming | oop | attribute | property | properties | getter | setter | access modifier | this | derived | default | constructor | static | overload | coupled | loose | tight',
+
+'https://lms.techelevator.com/cohorts/535/blocks/9/content_files/01_Classes_and_Encapsulation/01-introduction.md |
+https://docs.oracle.com/javase/tutorial/java/package/namingpkgs.html'
+),
+(
+'Inheritance',
+
+'One OOP principle is Inheritance, which is the act of having one class adopt the properties and methods of another class. This prevents code duplication and allows you to share code across classes while having the source code live in only one class file.',
+
+'object-oriented-programming | oop | inheritance | superclass | subclass | super | override | extend',
+
+'https://lms.techelevator.com/cohorts/535/blocks/9/content_files/02_Inheritance/01-inheritance.md |
+https://docs.oracle.com/en/java/javase/11/docs/api/java.base/java/lang/String.html |
+https://docs.oracle.com/en/java/javase/11/docs/api/java.base/java/util/ArrayList.html |
+https://docs.oracle.com/en/java/javase/11/docs/api/java.base/java/lang/Object.html'
+),
+(
+'Polymorphism',
+
+'In object-oriented programming, polymorphism is the idea that something can be assigned a different meaning or usage based on its context. This specifically allows variables and objects to take on more than one form. Often, you want to write code that can make a basic request, and based on context, allow different behaviors to take place. Polymorphism means you can write one generic method call that uses whatever specific version of that method is appropriate when it runs.',
+
+'object-oriented-programming | oop | polymorphism | code smell | loosely coupled | interface | implement',
+
+'https://lms.techelevator.com/cohorts/535/blocks/9/content_files/03_Polymorphism/01-whats-polymorphism.md |
+https://blog.codinghorror.com/code-smells/'
+),
+(
+'Managing Inheritance',
+
+'When you''re designing a new class, you can require other classes to implement certain methods, prevent other classes from overriding certain methods, and control which properties and methods child classes can access. Doing those things is an important way to communicate with other developers and helps ensure they use your code correctly. An abstract class is a class that you can''t create an instance of. An abstract method is a method with no implementation that''s declared in an abstract class. A final class is a class that other classes can''t inherit from. A final method is a method that a child class can''t override.',
+
+'abstract | final | protected | access | concrete | polymorphism',
+
+'https://lms.techelevator.com/cohorts/535/blocks/9/content_files/04_Managing_Inheritance/01-introduction.md'
 );
