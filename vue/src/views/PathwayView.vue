13c6764c91dcbdafc4b1fbb36c0d43c23f0510ca
Rox <r.valdina@gmail.com>
2023-12-08 10:00:16 -0500

    bones for pathway view started

--- vue/src/views/PathwayView.vue
+++ vue/src/views/PathwayView.vue
@@ -1,3 +1,23 @@
 <template>
-    <h1></h1>
-</template>
+    <div class="home grid-container">
+      <!-- ... other elements in the homeview ... -->
+  
+      <!-- Middle Row - Pathway View -->
+      <div class="grid-item pathway-view">
+        <div class="pathways-header">
+          <h2>Pathways</h2>
+        </div>
+        <div class="pathways-content">
+          <div class="pathway-item" v-for="pathway in pathways" :key="pathway.id">
+            <div class="pathway-title">{{ pathway.title }}</div>
+            <!-- Additional pathway content can go here -->
+          </div>
+        </div>
+        <div class="pathways-footer">
+          <input type="text" placeholder="Input Routes Back to homeview">
+        </div>
+      </div>
+  
+      <!-- ... remaining elements in the homeview ... -->
+    </div>
+  </template>
